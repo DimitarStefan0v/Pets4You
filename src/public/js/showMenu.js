143f79e6f460a141007bc8adfb935656d85ad424
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-09 22:13:45 +0200

    Change menu links display style from none to flex on menu icon click

--- src/public/js/showMenu.js
+++ src/public/js/showMenu.js
@@ -1,5 +1,11 @@
 const menuBtn = document.getElementById('menu-icon');
+const links = document.getElementById('menu-links');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('close');
+    if (menuBtn.classList.contains('close')) {
+        links.style.display = 'flex';
+    } else {
+        links.style.display = 'none';
+    }
 });
