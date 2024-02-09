9f03916c32608c4c44fa2ca3189fd5551835b745
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-09 22:28:53 +0200

    Add event listener and on the backdrop to close it if's clicked on it

--- src/public/js/showMenu.js
+++ src/public/js/showMenu.js
@@ -1,11 +1,24 @@
 const menuBtn = document.getElementById('menu-icon');
 const links = document.getElementById('menu-links');
+const backdrop = document.getElementById('backdrop');
 
 menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('close');
-    if (menuBtn.classList.contains('close')) {
+    menuBtn.classList.toggle('visible');
+
+    if (menuBtn.classList.contains('visible')) {
         links.style.display = 'flex';
+        backdrop.style.display = 'block'
     } else {
         links.style.display = 'none';
+        backdrop.style.display = 'none';
     }
+});
+
+backdrop.addEventListener('click', () => {
+    backdrop.style.display = 'none';
+
+    if (menuBtn.classList.contains('visible')) {
+        links.style.display = 'none';
+        menuBtn.classList.remove('visible');
+    } 
 });
