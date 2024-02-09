688bc26921ef2d3fc685e35b289c1e183557e8bd
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-09 18:05:23 +0200

    Add helmet for extra protection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
+const expressConfig = require('./config/expressConfig');
+const ejsConfig = require('./config/ejsConfig');
+
+const routes = require('./routes');
+
+const PORT = 5000;
+
 const app = express();
 
-const PORT = 5005;
+expressConfig(app);
+ejsConfig(app);
 
-app.get('/', (req, res) => {
-    res.send('Response from Express');
-});
+app.use(routes);
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
 const express = require('express');
+const helmet = require('helmet');
 
 
 const expressConfig = (app) => {
-	app.use(express.urlencoded({ extended: false }));
     app.use(express.static(path.resolve(__dirname, '..', 'public')));
+	app.use(express.urlencoded({ extended: false }));
+    app.use(helmet());
 };
 
 module.exports = expressConfig;
