41047b4f6b0681cc3cac8276c6126a06440de4f9
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-10 20:22:36 +0200

    Add users controller and endpoing for register on get method

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
+const usersController = require('./controllers/usersController');
 
 router.use(homeController);
+router.use('/auth', usersController);
 
 module.exports = router;
