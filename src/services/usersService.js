4739376b21aa54e8777a318b4c0ca9b2a4419c19
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-20 15:16:45 +0200

    Fix bug when checking if email is available

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const { VALIDATION_MESSAGES } = require('../utils/messages');
 
 exports.isEmailAvailable = async (email) => {
-	const user = await User.find({ email: email }).lean();
+	const user = await User.findOne({ email: email }).lean();
 
 	if (user) {
 		throw Error(VALIDATION_MESSAGES.EMAIL_TAKEN);
