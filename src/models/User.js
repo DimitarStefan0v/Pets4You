1bd5ecf1b40824c27f7df8b3d0c46fb23f9f8338
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-20 12:41:13 +0200

    Add validation on user schema

--- src/models/User.js
+++ src/models/User.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String,
+        trim: true,
+        required: true,
+        minLength: 3,
+        maxLength: 30
     },
     password: {
-        type: String
+        type: String,
+        trim: true,
+        required: true,
+        minLength: 6,
     },
     email : {
-        type: String
+        type: String,
+        trim: true,
+        required: true,
     }
 
 });
