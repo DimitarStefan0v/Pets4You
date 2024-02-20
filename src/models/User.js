877ecb4f3bb5b6e2df87afe114388a6ba21b40b6
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-20 13:57:55 +0200

    Add messages to user schema validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,25 +1,26 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-        minLength: 3,
-        maxLength: 30
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: true,
-        minLength: 6,
-    },
-    email : {
-        type: String,
-        trim: true,
-        required: true,
-    }
+const { VALIDATION_MESSAGES } = require('../utils/messages');
 
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		trim: true,
+		required: [true, VALIDATION_MESSAGES.REQUIRED('Името')],
+		minLength: [3, VALIDATION_MESSAGES.MIN_LENGTH('Името', 3)],
+		maxLength: [30, VALIDATION_MESSAGES.MAX_LENGTH('Името', 30)],
+	},
+	password: {
+		type: String,
+		trim: true,
+		required: [true, VALIDATION_MESSAGES.REQUIRED('Паролата')],
+		minLength: [6, VALIDATION_MESSAGES.MIN_LENGTH('Паролата', 6)],
+	},
+	email: {
+		type: String,
+		trim: true,
+		required: [true, VALIDATION_MESSAGES.REQUIRED('Имейла')],
+	},
 });
 
 const User = mongoose.model('User', userSchema);
