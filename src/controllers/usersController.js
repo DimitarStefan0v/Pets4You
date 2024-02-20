bbec27088dcd46b45c83f341f6d8739911f53615
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-20 14:34:20 +0200

    Add endpoint no register for post method

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
+const usersService = require('../services/usersService');
+
 const router = express.Router();
 
 router.get('/register', (req, res) => {
 	res.render('users/register', { pageTitle: 'Register' });
+});
+
+router.post('/register', async (req, res) => {
+	const { name, email, password, repeatPassword } = req.body;
+
+	try {
+		await usersService.isEmailAvailable(email);
+		await usersService.register(name, email, password, repeatPassword);
+	} catch (error) {
+		console.log(error);
+	}
+
+	res.redirect('/');
 });
 
 module.exports = router;
