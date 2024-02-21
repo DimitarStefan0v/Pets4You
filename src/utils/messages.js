f3b9afe8b57b61d09df57b6a14394cea02ef73c9
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-21 18:03:57 +0200

    Add new const message for name validation

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,7 +1,8 @@
 exports.VALIDATION_MESSAGES = {
-    REQUIRED: (field) => `${field} е задължително`,
-    MIN_LENGTH: (field, length) => `${field} не може да е по-малко от ${length} символа`,
-    MAX_LENGTH: (field, length) => `${field} не може да е по-дълго от ${length} символа`,
-    EMAIL_TAKEN: 'Имейлът е вече регистриран от друг потребител',
-    PASSWORDS_MISSMATCH: 'Полетата за парола и повтори парола не съвпадат'
+    REQUIRED: (field) => `Полето за ${field} е задължително.`,
+    MIN_LENGTH: (field, length) => `${field} не може да е по-малко от ${length} символа.`,
+    MAX_LENGTH: (field, length) => `${field} не може да е по-дълго от ${length} символа.`,
+    EMAIL_TAKEN: 'Имейлът е вече регистриран от друг потребител.',
+    PASSWORDS_MISSMATCH: 'Полетата за парола и повтори парола не съвпадат.',
+    NAME_REGEX: 'За ваше име трябва да въведете само едно име на български или английски, без цифри или интервали.' 
 };
