aeea1fc5c69af034fb6a93f435d0140322c283c1
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-21 19:05:11 +0200

    Add endpoint for 404

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
 	res.render('home/home', { pageTitle: 'Home', path: '/' });
 });
 
+router.get('/404', (req, res) => {
+    res.render('home/404', { pageTitle: 'Not Found', path: '/404' });
+});
+
 module.exports = router;
