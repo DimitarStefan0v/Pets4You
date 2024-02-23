cba2bfd2211c5d9687d1ef665b43f7623776b5a1
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-23 08:28:11 +0200

    Add login function in users service and check if users exist if not throw error

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,6 +1,21 @@
 const User = require('../models/User');
 const { VALIDATION_MESSAGES } = require('../utils/messages');
 
+exports.register = ({ name, email, password, repeatPassword }) =>
+	User.create({ name, email, password, repeatPassword });
+
+exports.login = async ({ email, password }) => {
+	const user = await User.findOne({ email: email }).lean();
+
+    if (!user) {
+        throw Error(VALIDATION_MESSAGES.INVALID_LOGIN);
+    }
+
+    // TODO: validate password with bcrypt
+
+    // TODO: create token with jwt and return it
+};
+
 exports.isEmailAvailable = async (email) => {
 	const user = await User.findOne({ email: email }).lean();
 
@@ -8,6 +23,3 @@ exports.isEmailAvailable = async (email) => {
 		throw Error(VALIDATION_MESSAGES.EMAIL_TAKEN);
 	}
 };
-
-exports.register = ({ name, email, password, repeatPassword }) =>
-	User.create({ name, email, password, repeatPassword });
