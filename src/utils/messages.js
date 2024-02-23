908b1f7c665e6384943a789e63e1d22655824667
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-23 08:21:34 +0200

    Add message for invalid login

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,9 +1,10 @@
 exports.VALIDATION_MESSAGES = {
-    REQUIRED: (field) => `Полето за ${field} е задължително.`,
+    REQUIRED: (field) => `Моля, въведете ${field}.`,
     MIN_LENGTH: (field, length) => `${field} не може да е по-малко от ${length} символа.`,
     MAX_LENGTH: (field, length) => `${field} не може да е по-дълго от ${length} символа.`,
     EMAIL_TAKEN: 'Имейлът е вече регистриран от друг потребител.',
     PASSWORDS_MISSMATCH: 'Полетата за парола и повтори парола не съвпадат.',
     INVALID_NAME: 'За ваше име трябва да въведете само едно име на български или английски, без цифри или интервали.',
     INVALID_EMAIL: 'Невалиден имейл',
+    INVALID_LOGIN: 'Невалиден имейл адрес или парола. Моля, опитайте отново.'
 };
