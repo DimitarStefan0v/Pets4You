9fc7add5c6c207d470d7723f848039c5a5c5d516
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-25 16:32:53 +0200

    Install and add cookie-parser middleware

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
 
 const expressConfig = (app) => {
     app.use(express.static(path.resolve(__dirname, '..', 'public')));
 	app.use(express.urlencoded({ extended: false }));
     app.use(helmet());
+    app.use(cookieParser());
 };
 
 module.exports = expressConfig;
