13029dfa9e5643351c8206de1c99e3ead74ec95e
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-25 17:38:05 +0200

    Add auth function as a global middleware

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -5,12 +5,14 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv').config();
 
+const { auth } = require('../middlewares/authMiddleware');
 
 const expressConfig = (app) => {
     app.use(express.static(path.resolve(__dirname, '..', 'public')));
 	app.use(express.urlencoded({ extended: false }));
     app.use(helmet());
     app.use(cookieParser());
+    app.use(auth);
 };
 
 module.exports = expressConfig;
