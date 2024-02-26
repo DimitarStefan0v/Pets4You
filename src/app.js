4d8fe2fd08b101c937db4d53984cf53bffb49ced
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-26 21:24:11 +0200

    Include the seed function after connecting to the db in the starting module

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const expressConfig = require('./config/expressConfig');
 const ejsConfig = require('./config/ejsConfig');
 const dbConnect = require('./config/dbConfig');
 
+const { seedInitialCategories } = require('./services/categoriesService');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,9 +14,16 @@ expressConfig(app);
 ejsConfig(app);
 
 dbConnect()
-	.then(() => console.log('DB connected successfully'))
+	.then(() => {
+		console.log('DB connected successfully');
+		seedInitialCategories()
+			.then((res) => console.log(res))
+			.catch((err) => console.log('Seeder error', err));
+	})
 	.catch((err) => console.log('DB error', err));
 
 app.use(routes);
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server is running on port ${process.env.PORT || 5000}`));
+app.listen(process.env.PORT || 5000, () =>
+	console.log(`Server is running on port ${process.env.PORT || 5000}`)
+);
