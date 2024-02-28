f0004cc465a0dd086cf4a7b111c85fc697fb7983
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-28 19:26:51 +0200

    Add add-post endpoint for get action

--- src/controllers/petsController.js
+++ src/controllers/petsController.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 router.get('/search', (req, res) => {
-    res.render('pets/search', { pageTitle: 'Pets', path: '/search' });
+	res.render('pets/search', { pageTitle: 'Домашни любимци', path: '/search' });
+});
+
+router.get('/add-post', (req, res) => {
+    res.render('pets/add-post', { pageTitle: 'Добави обява', path: '/add-post'})
 });
 
 module.exports = router;
