e1618992c02c5ba22ee46dd806755740d8a6aae0
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-29 19:57:27 +0200

    Add endpoint for add-post on post action

--- src/controllers/petsController.js
+++ src/controllers/petsController.js
@@ -5,7 +5,21 @@ router.get('/search', (req, res) => {
 });
 
 router.get('/add-post', (req, res) => {
-    res.render('pets/add-post', { pageTitle: 'Добави обява', path: '/add-post'})
+	res.render('pets/add-post', { pageTitle: 'Добави обява', path: '/add-post' });
+});
+
+router.post('/add-post', (req, res) => {
+	const data = {
+        name: req.body.name,
+        years: req.body.years,
+        months: req.body.months,
+        ownerFirstName: req.user.firstName,
+        ownerLastName: req.user.lastName,
+    };
+
+	console.log(data);
+
+	res.render('pets/add-post', { pageTitle: 'Добави обява', path: '/add-post' });
 });
 
 module.exports = router;
