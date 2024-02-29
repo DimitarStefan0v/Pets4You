db9b882247e5609dcc4c8c14c8fa8a44cf5a43a2
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-02-29 19:54:24 +0200

    Small change in auth middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,20 +5,21 @@ exports.auth = async (req, res, next) => {
 
 	if (token) {
 		try {
-			const decodedToken = await jwtPromises.verify(token, process.env.JWT_SECRET);
+			const decodedToken = await jwtPromises.verify(
+				token,
+				process.env.JWT_SECRET
+			);
 
 			req.user = decodedToken;
 			res.locals.user = decodedToken;
 			res.locals.isAuthenticated = true;
-
-			next();
 		} catch (error) {
 			res.clearCookie('auth');
 			res.redirect('/auth/login');
 		}
-	} else {
-		next();
 	}
+
+	next();
 };
 
 exports.isAuth = (req, res, next) => {
